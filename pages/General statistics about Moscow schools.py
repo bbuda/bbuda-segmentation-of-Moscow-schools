4419385158f942f7ed82538fa8c4d813e1ef55eb
Alexei Vashkevich <aleksvkv95@gmail.com>
2024-01-17 01:27:36 +0300

    rename hello

--- pages/General statistics about Moscow schools.py
+++ pages/General statistics about Moscow schools.py
@@ -3,5 +3,18 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import altair as alt
+import plotly.express as px
 
+st.set_page_config(
+    page_title="Welcome",
+    page_icon="👋",
+)
+df = pd.read_csv('C:\\Users\\aleks\\EGE.csv')
+st.dataframe(df)
+pca = pd.read_csv('C:\\Users\\aleks\\Pca_ege.csv')
+st.dataframe(pca)
+pca['Метка кластера'] = pca['Метка кластера'].astype(str)
 
+fig = px.scatter(pca, x="c1", y="c2", color="Метка кластера")
+st.plotly_chart(fig, theme="streamlit", use_container_width=True)
